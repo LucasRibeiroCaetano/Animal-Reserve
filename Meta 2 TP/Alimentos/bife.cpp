31339dd6532bed77aac9062af92f5a6d7ded0fa6
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2022-12-28 11:46:29 +0000

    Update bife.cpp

--- Meta 2 TP/Alimentos/bife.cpp
+++ Meta 2 TP/Alimentos/bife.cpp
@@ -1,9 +1,14 @@
 #include "bife.h"
 
-bife::bife() {
-
+Bife:: Bife(char nome, int vNutritivo, int toxicidade, int duracao, vector<string> cheiros): Alimento(nome, vNutritivo, toxicidade, duracao, cheiros){
+    nome = 'b';
+    vNutritivo = 10; // diminui 1 a cada instante
+    toxicidade = 2; //constante
+    duracao = 30;
+    cheiros[0] = 'carne'; //Constante - Tem sempre cheiro mesmo nao tendo valor nutritivo
+    cheiros[1] = 'ketchup';
 }
 
-bife::~bife() {
+Bife::~Bife() {
 
 }
