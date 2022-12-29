7c3779b791224af3823f8cc7cd8e6df4c5d16960
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2022-12-29 21:42:41 +0000

    Update ovelha.cpp

--- Meta 2 TP/Animais/ovelha.cpp
+++ Meta 2 TP/Animais/ovelha.cpp
@@ -1,26 +1,34 @@
 #include "ovelha.h"
 
-ovelha::ovelha() {
+Ovelha:: Ovelha(int id, string nome, int linha, int coluna): Animal(id, nome, linha, coluna){
+    especie = "ovelha";
+    representacao = 'O';
+    peso = (rand() % 8) + 4;
+    saude = 30;
+    fome = 0;
+    tempoVida = 35;
+    cheirosAgradaveis[0] = "erva";
+    cout << "Ovelha '" << nome << "' de id [" << id << "] foi criada." << endl;
+}
+
+
+Ovelha::~Ovelha() {
 
 }
 
-ovelha::~ovelha() {
-
-}
-
-void ovelha::movimento() {
+void Ovelha::movimento() {
 
     //animal::movimento();
 
 }
 
-void ovelha::alimentacao() {
+void Ovelha::alimentacao() {
 
     //animal::alimentacao();
 
 }
 
-void ovelha::interacao() {
+void Ovelha::interacao() {
 
     //animal::interacao();
 
