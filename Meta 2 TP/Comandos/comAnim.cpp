b9de2cf54e98cce4ac908669ad26dc6fe2def842
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-01-07 12:54:37 +0000

    Update comAnim.cpp

--- Meta 2 TP/Comandos/comAnim.cpp
+++ Meta 2 TP/Comandos/comAnim.cpp
@@ -1,5 +1,8 @@
-#include "comAnim.h"
-
 void comAnim::execute(const string &comando) {
+    int n = contaPalavras(comando);
+    if(n != 1)
+        cout << "Demasiados argumentos...";
+    else{
 
+    }
 }
